fd8ede876e70da26efddef2199b60dcf5d57b578
tanmaypardeshi <tanmaypardeshi@gmail.com>
2020-10-27 22:21:04 +0530

    Added get requests

--- apis/__init__.py
+++ apis/__init__.py
@@ -1,12 +1,21 @@
+import json, os
 from flask import Flask
 from flask_cors import CORS
 from flask_restful import Api
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
+with open(os.getcwd() + '/apis/db.json') as file:
+    details = json.load(file)
 
 app.config['SECRET_KEY'] = '5a8829076c843bf7474885f1a5d88c31'
+app.config['MYSQL_HOST'] = details['host']
+app.config['MYSQL_USER'] = details['user']
+app.config['MYSQL_PASSWORD'] = details['password']
+app.config['MYSQL_DB'] = details['db']
 api = Api(app)
+mysql = MySQL(app)
 cors = CORS(app, resources={
     r"/*": {
         "origins": "*"
--- apis/views.py
+++ apis/views.py
@@ -1,10 +1,133 @@
-from apis import api
+from apis import api, mysql
 from flask_restful import Resource
 
 
-class HelloWorld(Resource):
+class GetAllMatches(Resource):
     def get(self):
-        return {"data": "Hello"}
+        try:
+            message = ''
+            home_team = ''
+            away_team = ''
+            winner_team = ''
+            all_matches = []
+            objects = {}
+            cursor = mysql.connection.cursor()
+            get_match = cursor.execute("SELECT * FROM matches order by date_played desc")
+            if get_match > 0:
+                matches = cursor.fetchall()
+
+            for i in range(get_match):
+                cursor.execute("SELECT team_name from team where team_id=%s", (matches[i][1],))
+                home_team = cursor.fetchall()
+                cursor.execute("SELECT team_name from team where team_id=%s", (matches[i][2],))
+                away_team = cursor.fetchall()
+                cursor.execute("SELECT team_name from team where team_id=%s", (matches[i][3],))
+                winner_team = cursor.fetchall()
+                cursor.execute(
+                    "SELECT score, no_of_wickets, isFirstInnings from match_details where match_id=%s and team_id=%s"
+                    , (matches[i][0], matches[i][1]))
+                get_match_details1 = cursor.fetchall()
+                team1 = {
+                    'first': get_match_details1[0][2],
+                    'wickets': get_match_details1[0][1],
+                    'score': get_match_details1[0][0]
+                }
+                cursor.execute(
+                    "SELECT score, no_of_wickets, isFirstInnings from match_details where match_id=%s and team_id=%s"
+                    , (matches[i][0], matches[i][2]))
+                get_match_details2 = cursor.fetchall()
+                team2 = {
+                    'first': get_match_details2[0][2],
+                    'wickets': get_match_details2[0][1],
+                    'score': get_match_details2[0][0]
+                }
+                difference = team1['score'] - team2['score']
+                if team1['first'] == 1:
+                    if difference > 0:
+                        message = f"Team {winner_team[0][0]} won by {difference} runs."
+                    else:
+                        message = f"Team {winner_team[0][0]} won by {10 - team2['wickets']} wickets."
+                else:
+                    if difference > 0:
+                        message = f"Team {winner_team[0][0]} won by {10 - team1['wickets']} wickets."
+                    else:
+                        message = f"Team {winner_team[0][0]} won by {difference} runs."
+                objects["Match ID"] = matches[i][0]
+                objects["Home Team"] = home_team[0][0]
+                objects["Away Team"] = away_team[0][0]
+                objects["Winner Team"] = winner_team[0][0]
+                objects["Winning score/wicket"] = message
+                all_matches.append(objects)
+                objects = {}
+            return all_matches, 200
+        except Exception as e:
+            return {"error": e.__str__()}, 500
+        finally:
+            cursor.close()
 
 
-api.add_resource(HelloWorld, "/")
+class GetMatch(Resource):
+    def get(self, id):
+        try:
+            cursor = mysql.connection.cursor()
+            rs = cursor.execute("SELECT * from matches where match_id=%s", (id,))
+            if rs > 0:
+                match = cursor.fetchall()
+            match_id = match[0][0]
+            cursor.execute("SELECT * from team where team_id=%s", (match[0][1],))
+            team1 = get_team_detail(cursor.fetchall(), match_id)
+            cursor.execute("SELECT * from team where team_id=%s", (match[0][2],))
+            team2 = get_team_detail(cursor.fetchall(), match_id)
+            cursor.execute("SELECT team_name from team where team_id=%s", (match[0][3],))
+            winner_team = cursor.fetchall()[0][0]
+            man_of_the_match = match[0][4]
+            date_played = match[0][5]
+            return {
+                'Match ID': match_id,
+                'Winner Team': winner_team,
+                'Man Of The Match': man_of_the_match,
+                'Date of Match': str(date_played),
+                'team1': team1,
+                'team2': team2
+            }, 200
+        except Exception as e:
+            return {
+                       "error": e.__str__()
+                   }, 500
+        finally:
+            cursor.close()
+
+
+def get_team_detail(data, match_id):
+    try:
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT COUNT(*) from match_details where team_id=%s", (data[0][0], ))
+        matches_played = cursor.fetchall()[0][0]
+        cursor.execute("""SELECT * from match_details
+                        where team_id=%s and match_id=%s""", (data[0][0], match_id))
+        data = cursor.fetchall()
+        fours = data[0][2]
+        sixes = data[0][3]
+        wickets = data[0][4]
+        score = data[0][5]
+        team = {'Team ID': data[0][0],
+                'Team Name': data[0][1],
+                'Coach Name': data[0][2],
+                'Captain Name': data[0][3],
+                'Matches Played': matches_played,
+                'Number of Fours': fours,
+                'Number of Sixes': sixes,
+                'Number of Wickets Lost': wickets,
+                'Score': score
+                }
+        return team
+    except Exception as e:
+        return {
+            "error": e.__str__()
+        }
+    finally:
+        cursor.close()
+
+
+api.add_resource(GetAllMatches, "/getallmatches")
+api.add_resource(GetMatch, "/getmatch/<int:id>")
